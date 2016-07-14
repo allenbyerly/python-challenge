210d4b7e2c601e32cb5ab155993e83c16928d694
Allen Byerly <allenbyerly@gmail.com>
2016-07-14 01:28:17 -0700

    update

--- python-challenge.py
+++ python-challenge.py
@@ -1,53 +1,15 @@
 
 # collect and count customers and store the variables as collections where the count of each charecters name is stored
 # as a pair...
-
-path = '/Shared/Projects/python-challenge/customer_products.txt'
-
-#set path to find the discount list.  Set Path to:
-# /Shared/Projects/python-challenge/customer_products.txt
-def product_assignments(path):
- #   from collections import deque
- from collections import deque
- from functools import reduce
-# open the file and put in a while loop
-file = open(path, 'r')
-
-text = file.readline()
-while (len(text) > 0):
-# Split each line by the semicolon to sepaate out products from customers
- split = text.split(";")
-
-# Split all collected customers]9
- customers = split[0].split(",")
- print customers
-
- products = split[1].split(",")
- print products
-
-
- #Step One:
-
-
- customer_values = dict((a, len(a)) for a in customers)
-
- print customer_values.values()
- text = file.readline()
-
- # Check if e3. If the number of letters in the product's name share s any commo factors (besides 1) with
- # the
- # number of letters in
- # the customer's name then the SS is multiplied by 1.5.
-
-#1. If the number of letters in the product's name is even then the SS is the number of vowels (a, e, i, o, u, y2) in the customer's name multiplied by 1.5.
-
 def set_ss(customers, products):
-
+    ss = {}
+    multiplier = 1
     for product in products:
         for customer in customers:
 
-            x = count_letters(product)
-            y = count_letters(customer)
+
+            x = count_letters(product.strip())
+            y = count_letters(customer.strip())
             common_factor = hcf(x,y)
 
             if common_factor > 1: multipler = 1.5
@@ -67,10 +29,13 @@ def set_ss(customers, products):
             # is the number of vowels (a, e, i, o, u, y) in the customer's name
             # multiplied by 1.5.
             if is_even(product):
-                ss[customer] = 1.5 * count_vowels(customer) * multiplier
+                ss[customer, product] = 1.5 * count_vowels(customer) * multiplier
             else:
-                ss[customer] = 1 * count_consonants(customer) * multiplier
+                ss[customer, product] = 1 * count_consonants(customer) * multiplier
 
+    print ss.values()
+
+#def get_score(counter):
 
 
 
@@ -92,35 +57,50 @@ def hcf(x, y):
 
    return
 
+def rule_one(x, y):
 
-def is_even(input):
-    if (len(input) % 2 == 0): return true
-    else: return false
+    if (len(y) % 2 == 0): return True
+    else: return False
 
-def count_letters(input):
-    return len(input)
+def is_even(value):
+    if (len(value) % 2 == 0): return True
+    else: return False
 
-#This function takes in an inut string and returns the number of consonants contained within it.
-def count_consonants(input):
-    consonants = "bcdfghjklmnpqrstvwxz"
-    count = 0
-    input = input.lower()
-    for i in string:
-        if i in consonants:
-            count +=1
+def count_letters(value):
+    return len(value)
 
-    return count
+
+
+for product in products:
+    for customer in customers:
+
+        #This function takes in an inut string and returns the number of consonants contained within it.
+def vcv(x, y):
+    if (len(y) % 2 == 0):
+        vowels = "aeiouy"
+        count = 0
+        x = x.lower()
+        for i in x:
+            if i in vowels:
+                count +=1
+
+        return count * 1.5
+    else:
+        consonants = "bcdfghjklmnpqrstvwxz"
+        count = 0
+        x = x.lower()
+        for i in x:
+            if i in consonants:
+                count +=1
+
+        return count
 
 #This function takes in an inut string and returns the number of vowels contained within it.
-def count_vowels(input):
-    vowels = "aeiouy"
-    count = 0
-    input = input.lower()
-    for i in input:
-        if i in vowels:
-            count +=1
+def count_vowels(value):
+
 
     return count
+
 
 
 
@@ -132,3 +112,49 @@ def count_vowels(input):
    #automatically check and select the he first
     # available product.  Then check all other available products for a higher
     # score.customer will check every available product by first checking and selecting the first avail  select the highest score
+
+
+
+path = '/Shared/Projects/python-challenge/customer_products.txt'
+
+#set path to find the discount list.  Set Path to:
+# /Shared/Projects/python-challenge/customer_products.txt
+def product_assignments(path):
+ #   from collections import deque
+ from collections import deque
+ from functools import reduce
+# open the file and put in a while loop
+file = open(path, 'r')
+counter = 0;
+
+text = file.readLines()
+counter += counter
+
+
+while (len(text) > 0):
+# Split each line by the semicolon to sepaate out products from customers
+ split = text.split(";")
+
+# Split all collected customers]9
+ customers = split[0].split(",")
+ print customers
+
+ products = split[1].split(",")
+ print products
+
+
+ #Step One:
+
+
+ customer_values = dict((a, len(a)) for a in customers)
+
+ print customer_values.values()
+ set_ss(customers, products)
+ text = file.readline()
+
+ # Check if e3. If the number of letters in the product's name share s any commo factors (besides 1) with
+ # the
+ # number of letters in
+ # the customer's name then the SS is multiplied by 1.5.
+
+#1. If the number of letters in the product's name is even then the SS is the number of vowels (a, e, i, o, u, y2) in the customer's name multiplied by 1.5.

ebe201dd86624e01966289c345e7505f7d43dabf
OX <ox@MyBook.local>
2016-08-08 06:43:21 -0700

    Moved Classes into a new file called Market.py

---
+++ market.py
@@ -0,0 +1,99 @@
+from collections import deque
+from functools import reduce
+from importyools import re
+from itertools import product
+from string import whitespace
+from challenge2 import *
+
+class Customers:
+  def __init__(self, name):
+      self.name = name
+      self.id = name
+  def __repr__(self): return repr(self.id)
+
+
+class Products(object):
+  def __init__(self, name, merchant):
+      name = name
+      discount = discount
+      merchant = merchant
+      id = name + "_" + merchant
+  def __repr__(self): return repr(self.id)
+
+
+class Merchants:
+  def __init__(self, name):
+      name = name
+      id = name
+      product_discounts = [Products]
+  ## TO DO - provide function to get/set daily discount product list
+  def __repr__(self): return repr(self.id)
+
+#the dealer determines all the deals and assignments of products to customers
+class Dealers:
+  def __init__(self, name):
+      name = name
+      id = name
+  def __repr__(self): return repr(self.id)
+
+#offers are combinations of products and customers and have a ss determined on construction
+class Offers(object):
+  def __init__(self, customer, product):
+      self.name = [customer, product]
+      self.id = product(customers, products)
+      self.customer = customer
+      self.product = product
+      self.dealer = dealer
+      self.score = dealer.get_score(id)
+  def __repr__(self): return repr(self.id)
+
+class Deals(object):
+  def __init__(self, customer, product):
+      self.name = [customer, product]
+      self.id = product(customers, products)
+      self.customer = customer
+      self.product = product
+      self.dealer = dealer
+      self.score = dealer.get_score(id)
+  def __repr__(self): return repr(self.id)
+
+class Marketer:
+  def __init__(self, name):
+    self.name = name
+    self.customers = [Customers]
+    self.products = [Products]
+    self.all_offers = [Offers]
+    self.optimum_offers = [Offers]
+  def __repr__(self): return repr(self.name)
+ # def set_product(self, product)
+
+class Campaigns(object):
+  def __init__(self, customer, product):
+      self.customer = customer
+      self.product = product
+      self.name = [customer, product]
+      #Suitability Score
+      self.ss = 0
+
+  def __repr__(self): return repr((self.name, self.product, self.score))
+  def high_score(self): return self.high_score
+  def high_product(self): return self.high_product
+  def __repr__(self): return repr([self.name, self.score])
+
+  #all_offers = {x: dealer.get_score for x in product_map}
+  #class customer(object):
+  #    def __init__(self, name, product, score):
+  #        self.name = name
+  #        self.profession = profession
+  #        self.campaign_list = dict(product, campaign)
+
+  class Markets:
+      def __init__(self):
+          name = 'one little market'
+          id = name
+          merchants = list(Merchants())
+          products =  list(Products())
+          customers = list(Customers())
+
+          deal_match = self.dealer.get_best_match()
+      def __repr__(self): return repr(self.id)
